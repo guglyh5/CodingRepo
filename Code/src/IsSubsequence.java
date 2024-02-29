befc0e0e732d43f6b204d7d8c2eeee7109493d68
Jaspreet <jaspreet.singh1@axtria.com>
2024-02-29 11:29:02 +0530

    Bug Fixes

--- Code/src/IsSubsequence.java
+++ Code/src/IsSubsequence.java
@@ -1,3 +1,5 @@
+package Code.src;
+
 /*Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
 
 A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without 
